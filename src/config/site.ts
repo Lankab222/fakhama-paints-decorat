5a5d30f87202e409063e8e8a322e2938c5ea85cd
Lankab222 <m_lankab@hotmail.com>
2026-07-17 03:47:19 +0300

    uploud scama1

---
+++ src/config/site.ts
@@ -0,0 +1,25 @@
+export const siteConfig = {
+  siteName: "دهانات وديكورات الفخامة",
+
+  siteUrl: "https://alfakhama.site",
+
+  siteDescription:
+    "دهانات وديكورات الفخامة لتنفيذ الدهانات الداخلية والخارجية والديكورات والعوازل والمظلات والسواتر وأعمال الترميم في مكة المكرمة.",
+
+  phone: "+966543441970",
+
+  email: "info@alfakhama.site",
+
+  logo: "/uploads/logo2.webp",
+
+  businessImage: "/uploads/hero-image.webp",
+
+  address: {
+    addressLocality: "مكة المكرمة",
+    addressRegion: "مكة المكرمة",
+    postalCode: "12455",
+    addressCountry: "SA",
+  },
+
+  areaServed: ["مكة المكرمة"],
+};
